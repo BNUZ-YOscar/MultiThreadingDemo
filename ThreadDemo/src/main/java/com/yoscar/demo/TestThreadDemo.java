25360e6ca6eca5979e5e3eb0cfa39ef105f8f58f
yoscar <549953659@qq.com>
2021-03-04 10:26:53 +0800

    并发demo

--- ThreadDemo/src/main/java/com/yoscar/demo/TestThreadDemo.java
+++ ThreadDemo/src/main/java/com/yoscar/demo/TestThreadDemo.java
@@ -13,7 +13,6 @@ public class TestThreadDemo extends Thread {
         TestThreadDemo thread = new TestThreadDemo();
 
         thread.start();
-
         for (int i = 0; i < 20; i++) {
             System.out.println("主线程执行" + i);
         }
---
+++ ThreadDemo/src/main/java/com/yoscar/demo/runnableDemo/RunnableDemo.java
@@ -0,0 +1,22 @@
+package com.yoscar.demo.runnableDemo;
+
+public class RunnableDemo implements Runnable {
+
+    public void run() {
+        for (int i = 0; i < 20; i++) {
+            System.out.println("子线程" + i);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        RunnableDemo runnable = new RunnableDemo();
+
+        new Thread(runnable).start();
+
+        for (int i = 0; i < 20; i++) {
+            System.out.println("主线程：" + i);
+        }
+    }
+
+}
